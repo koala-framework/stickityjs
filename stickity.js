d3c68630e9ff8b82ee9f93c04749a7b22405599d
Michael Tiefenthaler <mt@vivid-planet.com>
2016-10-13 09:05:20 +0200

    Change UMD snippet

--- stickity.js
+++ stickity.js
@@ -1,30 +1,12 @@
-(function (factory) {
+;(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
         define(['jquery'], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node/CommonJS
-        module.exports = function( root, jQuery ) {
-            if ( jQuery === undefined ) {
-                // require('jQuery') returns a factory that requires window to
-                // build a jQuery instance, we normalize how we use modules
-                // that require this pattern but the window provided is a noop
-                // if it's defined (how jquery works)
-                if ( typeof window !== 'undefined' ) {
-                    jQuery = require('jquery');
-                }
-                else {
-                    jQuery = require('jquery')(root);
-                }
-            }
-            factory(jQuery);
-            return jQuery;
-        };
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('jquery'));
     } else {
-        // Browser globals
-        factory(jQuery);
+        root.YourModule = factory(root.jQuery);
     }
-}(function ($) {
+}(this, function ($) {
     var stickityContainer;
     var settings;
 
